9b0812c061e4d3c4d73be6e60af7a5b94e5c593a
Andy Williams <andy@nonissue.org>
2021-10-05 00:18:52 -0600

    Add husky, improve eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,48 @@
-// .eslintrc.js
+// https://github.com/lightsound/nexst-tailwind/blob/main/.eslintrc.js
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser", // the TypeScript parser we installed earlier
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: { jsx: true }, // Allows for the parsing of JSX
+    project: "./tsconfig.json",
   },
-  plugins: ["@typescript-eslint", "import", "react"],
+  plugins: ["@typescript-eslint", "import", "react", "tailwindcss"],
   extends: [
-    "eslint:recommended", // eslint default rules
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/eslint-recommended", // eslint TypeScript rules (github.com/typescript-eslint/typescript-eslint)
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended", // eslint react rules (github.com/yannickcr/eslint-plugin-react)
-    "plugin:jsx-a11y/recommended", // accessibility plugin
+    "eslint:recommended",
     "plugin:@next/next/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/typescript",
+    "plugin:import/warnings",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
   ],
   rules: {
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      { prefer: "type-imports" },
+    ],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "jsx-a11y/anchor-is-valid": "off",
     "@typescript-eslint/no-unused-vars": ["error"],
+    "import/newline-after-import": "error",
+    "import/no-default-export": 1,
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react/display-name": "error",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
   },
   settings: {
     "import/resolver": {
@@ -37,4 +55,42 @@ module.exports = {
     },
     files: ["**/*.ts", "**/*.tsx"],
   },
+  overrides: [
+    {
+      files: ["src/pages/**/*.tsx", "src/pages/api/**/*.ts"],
+      rules: {
+        "import/no-default-export": "off",
+        "@typescript-eslint/naming-convention": [
+          "error",
+          { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
+          {
+            selector: ["classProperty", "typeProperty", "method"],
+            format: ["camelCase"],
+          },
+          {
+            selector: "variable",
+            types: ["boolean"],
+            format: ["PascalCase"],
+            prefix: ["is", "has", "should"],
+          },
+        ],
+      },
+    },
+    {
+      files: ["src/type/**/*.d.ts"],
+      rules: {
+        "@typescript-eslint/naming-convention": [
+          "error",
+          { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
+          { selector: ["classProperty", "method"], format: ["camelCase"] },
+          {
+            selector: "variable",
+            types: ["boolean"],
+            format: ["PascalCase"],
+            prefix: ["is", "has", "should"],
+          },
+        ],
+      },
+    },
+  ],
 };
